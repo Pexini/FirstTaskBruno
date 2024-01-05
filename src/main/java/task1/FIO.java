238c3e842dc061c17a46d21d7223117f30875895
Pavel Davydov <Davydov.works@gmail.com>
2024-01-05 02:30:08 -0500

    First Task FIO

---
+++ src/main/java/task1/FIO.java
@@ -0,0 +1,13 @@
+package task1;
+
+import java.sql.SQLOutput;
+
+public class FIO {
+    public static void main(String[] args) {
+        String firstName = "Pavel";
+        String secondName = "Davydov";
+        String middelName = "Vladimirovich";
+        String phone = "+79999999999";
+        System.out.println("Имя студента: " + " " + firstName + " " + secondName + " " + middelName +" " + phone);
+    }
+}
