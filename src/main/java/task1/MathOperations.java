77b9f96ba7cb451e92808e2b61c85fbad4a65a71
Pavel Davydov <Davydov.works@gmail.com>
2024-01-05 02:35:27 -0500

    MathOperations

---
+++ src/main/java/task1/MathOperations.java
@@ -0,0 +1,13 @@
+package task1;
+
+public class MathOperations {
+    public static void main(String[] args) {
+        int a =10;
+        int b = 2;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
